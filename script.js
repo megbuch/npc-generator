2f2e1b42057346454da132f2b6c37b07a9c6a93c
bardIRL <115611931+bardIRL@users.noreply.github.com>
2022-10-31 00:09:45 -0700

    fixed pronouns on left side. randomized main info and family info on left side.

--- script.js
+++ script.js
@@ -237,14 +237,103 @@ const maleNames = [
   "Gladeus",
   "Ajax",
   "Pascal",
-  "",
 ];
 
-const lastNames = ["Stormborn", "Emerson", "Jensen", "Holloway"];
+const lastNames = [
+  "Stormborn",
+  "Emerson",
+  "Jensen",
+  "Holloway",
+  "Ambrose",
+  "Amberhide",
+  "Shadowhide",
+  "Ambers",
+  "Alinac",
+  "Belmont",
+  "Blackmark",
+  "Bellevue",
+  "Bertillon",
+  "Boudreaux",
+  "Bordeaux",
+  "Cinderhall",
+  "Longfellow",
+  "Clawroot",
+  "Cinderhell",
+  "Coldblight",
+  "Chauvet",
+  "Clearwater",
+  "Dawnless",
+  "Deepfellow",
+  "Featherly",
+  "Strider",
+  "Havenly",
+  "Keenstone",
+  "Brightstone",
+  "Lightfoot",
+  "Deeprider",
+  "Wilder",
+  "Wiser",
+  "Wisekeeper",
+  "Warmane",
+  "Brightwood",
+  "Katz",
+  "Laurier",
+  "Viotto",
+  "Trevino",
+  "Whitlock",
+  "Boone",
+  "Remington",
+  "Duke",
+  "Pierce",
+  "Steel",
+  "Steele",
+  "Vandenberg",
+  "Webb",
+  "Ellis",
+  "Presley",
+  "Damarys",
+  "Luza",
+  "Godfrey",
+  "Greathouse",
+  "Knightley",
+  "Princely",
+  "Prince",
+  "King",
+  "Gregorios",
+  "Coldshore",
+  "Gallus",
+  "Ratley",
+  "Sevenson",
+  "Vita",
+  "Darcey",
+  "Weston",
+  "Bradford",
+  "Fitzgerald",
+  "Stone",
+  "Tennsley",
+  "Hanover",
+  "Ryder",
+  "Atwood",
+  "Hamilton",
+  "Rowe",
+  "Wilkes",
+  "Bentley",
+  "Claymoore",
+  "Brighton",
+  "Bailey",
+  "Harrington",
+  "Larson",
+  "Holt",
+  "Pierson",
+  "Evershire",
+  "Crawford",
+  "Goodfellow",
+  "Harper",
+  "Sorrowful",
+  "Seaborn",
+];
 
 const ages = [
-  "Child",
-  "Juvenile",
   "Young Adult",
   "Young Adult",
   "Adult",
@@ -460,6 +549,48 @@ const jobs = [
   "Takes Odd Jobs",
 ];
 
+const sexualOrientation = [
+  "Straight",
+  "Straight",
+  "Straight",
+  "Straight",
+  "Straight",
+  "Straight",
+  "Straight",
+  "Straight",
+  "Straight",
+  "Straight",
+  "Straight",
+  "Straight",
+  "Straight",
+  "Straight",
+  "Bisexual",
+  "Asexual",
+  "Gay",
+];
+
+const relationshipStatus = [
+  "Single",
+  "Single",
+  "Single",
+  "In A Relationship",
+  "Married",
+];
+
+const children = [
+  "None",
+  "None",
+  "None",
+  "None",
+  "None",
+  "None",
+  1,
+  1,
+  2,
+  2,
+  3,
+  4,
+];
 //function for getting randomized sex
 function getRandomSex() {
   confirmedSex = sex[Math.floor(Math.random() * sex.length)];
@@ -520,11 +651,81 @@ function setRandomJob() {
   return (document.getElementById("job").innerText = getRandomJob());
 }
 
+//function for getting randomized orientation
+function getRandomOrientation() {
+  return sexualOrientation[
+    Math.floor(Math.random() * sexualOrientation.length)
+  ];
+}
+
+function setRandomOrientation() {
+  return (document.getElementById("orientation").innerText =
+    getRandomOrientation());
+}
+
+//function for getting randomized relationship status
+function getRandomRelationship() {
+  return relationshipStatus[
+    Math.floor(Math.random() * relationshipStatus.length)
+  ];
+}
+
+function setRandomRelationship() {
+  return (document.getElementById("relationship").innerText =
+    getRandomRelationship());
+}
+
+//function for getting randomized # of children
+function getRandomChildren() {
+  return children[Math.floor(Math.random() * children.length)];
+}
+
+function setRandomChildren() {
+  return (document.getElementById("children").innerText = getRandomChildren());
+}
+
+//function for changing pronouns from she/he
+
+function setOverall() {
+  if (confirmedSex == "female") {
+    return (document.getElementById("overall").innerText = "She");
+  } else {
+    return (document.getElementById("overall").innerText = "He");
+  }
+}
+
+function setFace() {
+  if (confirmedSex == "female") {
+    return (document.getElementById("face").innerText = "She");
+  } else {
+    return (document.getElementById("face").innerText = "He");
+  }
+}
+
+function setBody() {
+  if (confirmedSex == "female") {
+    return (document.getElementById("body").innerText = "She");
+  } else {
+    return (document.getElementById("body").innerText = "He");
+  }
+}
+
 //updates entire page on click of generate button
 document.querySelector(".generate").addEventListener("click", setRandomSex);
 document.querySelector(".generate").addEventListener("click", setRandomName);
 document.querySelector(".generate").addEventListener("click", setRandomAge);
 document.querySelector(".generate").addEventListener("click", setRandomRace);
 document.querySelector(".generate").addEventListener("click", setRandomJob);
-
+document
+  .querySelector(".generate")
+  .addEventListener("click", setRandomOrientation);
+document
+  .querySelector(".generate")
+  .addEventListener("click", setRandomRelationship);
 console.log(confirmedSex);
+document
+  .querySelector(".generate")
+  .addEventListener("click", setRandomChildren);
+document.querySelector(".generate").addEventListener("click", setOverall);
+document.querySelector(".generate").addEventListener("click", setFace);
+document.querySelector(".generate").addEventListener("click", setBody);
